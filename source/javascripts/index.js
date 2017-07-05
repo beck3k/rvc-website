b16634d8a31776a51a9c41322de9a70c602e4d3f
beck3k <physicalc123@gmail.com>
2017-07-05 17:16:06 -0400

    Add files via upload

---
+++ source/javascripts/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$('.white-links').children().children().addClass('black-text');
+	$('.slider').slider({height: 800});
+	$('.modal').modal();
+	$('.parallax').parallax();
+	$('nav.nav-extended').append('<div class = "nav-content white"><ul class = "tabs tabs-transparent"><li><a href = "#" class = "to-about">About</a></li><li><a href = "#" class = "to-history">History</a></li><li><a href = "#" class = "to-events">Events</a></li></ul></div>');
+	$('.footer-links').children().children().addClass('grey-text text-lighten-3');
+	$('.tabs').children().addClass('tab').children().addClass('black-text');
+	$('ul.tabs').tabs();
+	$('.to-about').click(function() {
+		$('html, body').animate({
+			scrollTop: $("#scroll-about").offset().top
+		}, 2000);
+	});
+	$('.to-history').click(function() {
+		$('html, body').animate({
+			scrollTop: $("#scroll-bhistory").offset().top
+		}, 2000);
+	});
+	$('.to-events').click(function() {
+		$('html, body').animate({
+			scrollTop: $("#scroll-events").offset().top
+		}, 2000);
+	});
+});
+
--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,28 +1,4 @@
 $(document).ready(function(){
 	$('.white-links').children().children().addClass('black-text');
-	$('.slider').slider({height: 800});
-	$('.modal').modal();
-	$('.parallax').parallax();
-	$('li.footer-links').each(function(){
-		$(this).addClass('grey-text text-lighten-3');
-	});
 	$('.footer-links').children().children().addClass('grey-text text-lighten-3');
-	$('.tabs').children().addClass('tab').children().addClass('black-text');
-	$('ul.tabs').tabs();
-	$('.to-about').click(function() {
-		$('html, body').animate({
-			scrollTop: $("#scroll-about").offset().top
-		}, 2000);
-	});
-	$('.to-history').click(function() {
-		$('html, body').animate({
-			scrollTop: $("#scroll-bhistory").offset().top
-		}, 2000);
-	});
-	$('.to-events').click(function() {
-		$('html, body').animate({
-			scrollTop: $("#scroll-events").offset().top
-		}, 2000);
-	});
 });
-
